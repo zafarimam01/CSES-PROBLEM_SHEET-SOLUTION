917af4eb1924994030c7996bf7105418baab91f0
MD ZAFAR <jafarimam775@gmail.com>
2026-06-23 23:57:00 +0530

    apartment solution cses

--- Introductory_Problems/stringReordering.cpp
+++ Introductory_Problems/stringReordering.cpp
@@ -1,1 +1,14 @@
 //stringReordering Solution upcoming .........;
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n=
+    cin >> n;
+    string c;
+    cin >> c;
+    for(int i=0; i < n ; i++){
+        f
+    }
+    return 0;
+}
--- Sorting-and-Searching_Solution/apartments.cpp
+++ Sorting-and-Searching_Solution/apartments.cpp
@@ -1,16 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+int solve(vector<int>& a, vector<int>& b, int n, int m, int k){
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    // ---> pointer to store current value 
+    int ptr1 = 0 , ptr2 = 0, ans = 0;
+    while(ptr1 < n && ptr2 < m){
+        if(b[ptr2] < a[ptr1] - k){ //
+            ptr2++;
+        }
+        else if(a[ptr1] < b[ptr2]){
+            ptr1++;
+        }
+        else {
+            ans += 1;
+            ptr1 += 1;
+            ptr2 += 1;;
+        }
+    }
+    return ans;
+}
 int main(){
     int n,m,k;
-    cin >> n,m,k; // n = number of applicants , m = of apartments , k = max diff
-    vector<int> applicant(n);
-    vector<int> apartment(m);
+    cin >> n >> m >>k; // n = number of applicants , m = of apartments , k = max diff
+    vector<int> a(n);
+    vector<int> b(m);
     for(int i = 0; i < n; i++){
-        cin >> applicant[i];
+        cin >> a[i];
     }
     for(int i = 0; i < m; i++){
-        cin >> apartment[i];
+        cin >> b[i];
     }
+    int result = solve(a,b,n,m,k);
+    cout << result << endl;
     
     return 0;
 }
