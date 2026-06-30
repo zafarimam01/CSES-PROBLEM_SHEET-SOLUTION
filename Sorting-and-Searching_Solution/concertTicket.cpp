d28a4534161cd0dc9be89abfb967b01e2e5b619a
MD ZAFAR <jafarimam775@gmail.com>
2026-06-30 23:22:22 +0530

    concert ticket solution

---
+++ Sorting-and-Searching_Solution/concertTicket.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main(){
+    int n,m;
+    cin >> n >> m;
+    multiset<ll>ms(n);
+    multiset<ll>sm(m);
+    //solution upcoming ---
+    return 0;
+}
