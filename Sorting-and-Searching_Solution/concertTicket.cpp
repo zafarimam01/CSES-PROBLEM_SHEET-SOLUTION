ca8e8ae168976059f89ee43e3ad273cc00cd666b
MD ZAFAR <jafarimam775@gmail.com>
2026-07-01 23:21:15 +0530

    concert ticket cses problem solution

--- Sorting-and-Searching_Solution/concertTicket.cpp
+++ Sorting-and-Searching_Solution/concertTicket.cpp
@@ -2,10 +2,28 @@
 #define ll long long
 using namespace std;
 int main(){
-    int n,m;
+    int n , m;
     cin >> n >> m;
-    multiset<ll>ms(n);
-    multiset<ll>sm(m);
-    //solution upcoming ---
+    multiset<ll>ms;
+    for(ll i=0; i<n; i++){
+        int x;
+        cin >> x;
+        ms.insert(x);
+    }
+    vector<ll>sm(m);
+    for(ll i=0; i<m; i++){
+        cin >> sm[i];
+    }
+    for(ll i=0; i<m; i++){
+        auto it = ms.upper_bound(sm[i]);
+        if(it==ms.begin()){
+            cout <<"-1"<<endl;
+        }
+        else{
+            --it;
+            cout<< *it <<endl;
+            ms.erase(it);
+        }
+    }
     return 0;
 }
